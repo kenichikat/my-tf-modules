65f46a36e5ba454bde22ea8cd1eee4bdad868030
Kenichi KATO <k3nichikato@gmail.com>
2024-03-07 18:18:12 +0900

    add output

--- modules/wp-lightsail/output.tf
+++ modules/wp-lightsail/output.tf
@@ -1,11 +1,15 @@
 output "lightsail_instance_name" {
-  value = aws_lightsail_instance.this.name
+    value = aws_lightsail_instance.this.name
 }
 
-output "lightsail_instance_public_ip" {
-  value = aws_lightsail_static_ip.this.ip_address
+output "lightsail_instance_public_ip_address" {
+    value = aws_lightsail_static_ip.this.ip_address
 }
 
-output "aws_lightsail_bucket_name" {
-  value = aws_lightsail_bucket.this.name
+output "lightsail_bucket_name" {
+    value = aws_lightsail_bucket.this.id
+}
+
+output "lightsail_bucket_region" {
+    value = aws_lightsail_bucket.this.region
 }
